87b6bcbace5e21608cd058434e742877a562aa96
bansaltushar014 <bansaltushar014@gmail.com>
2020-06-01 01:27:05 +0530

    build

---
+++ build/precache-manifest.18201649a3ff7330cdfcb8c24d1008a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad0f0cd06291dcd9b99b140f1e9f638d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1382675e975c8577799",
+    "url": "/static/css/main.6c5cef8e.chunk.css"
+  },
+  {
+    "revision": "fa09b02a0976e1357081",
+    "url": "/static/js/2.67d85336.chunk.js"
+  },
+  {
+    "revision": "3bbe45b9c3d976ac10b0f782bb54168c",
+    "url": "/static/js/2.67d85336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1382675e975c8577799",
+    "url": "/static/js/main.73123abb.chunk.js"
+  },
+  {
+    "revision": "328c9430560a082a6d3d",
+    "url": "/static/js/runtime-main.87a48f45.js"
+  }
+]);
