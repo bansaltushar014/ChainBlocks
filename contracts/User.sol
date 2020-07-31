67048dae254b38bf33559b78cc94ac3780840fee
bansaltushar014 <bansaltushar014@gmail.com>
2020-08-01 00:48:23 +0530

    Comments

--- contracts/User.sol
+++ contracts/User.sol
@@ -9,12 +9,13 @@ contract User {
 
   mapping(address => MyUser) myusers;
 
+  // Save the Hash of books bought by the Reader
   function set(string memory x) public {
     myusers[msg.sender].ipfs.push(x);
   }
   
-    function get() public view returns (string[] memory) {
-    
-     return myusers[msg.sender].ipfs;
+  // Get the Hashes of book bought by the Reader
+  function get() public view returns (string[] memory) {
+    return myusers[msg.sender].ipfs;
    }
 }
